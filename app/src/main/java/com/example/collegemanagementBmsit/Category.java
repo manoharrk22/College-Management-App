7af32f5394ea1bfcbd212dceaa435fefc7d7dc2f
manoharrk22 <manoharrk36@gmail.com>
2020-11-24 10:04:41 +0530

    Setting up Main Activity

---
+++ app/src/main/java/com/example/collegemanagementBmsit/Category.java
@@ -0,0 +1,6 @@
+package com.example.collegemanagementBmsit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Category extends AppCompatActivity {
+}
--- app/src/main/java/com/example/collegemanagementBmsit/MainActivity.java
+++ app/src/main/java/com/example/collegemanagementBmsit/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.collegemanagementBmsit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText emailLogin = (EditText) findViewById(R.id.editTextTextEmailLogin);
+        emailLogin.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button loginButton=(Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent=new Intent(MainActivity.this,Category.class);
+                startActivity(loginIntent);
+            }
+        });
+
     }
 }
