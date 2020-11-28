19edc156ee4befce6f5dcd0c072e6e88a3be1a04
suprit <supriths4685@gmail.com>
2020-11-28 07:09:33 +0530

    modified things

--- app/src/main/java/com/example/collegemanagementBmsit/MainActivity.java
+++ app/src/main/java/com/example/collegemanagementBmsit/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,19 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button adminButton=(Button) findViewById(R.id.activity_main_admin_btn);
+        CardView adminButton=(CardView) findViewById(R.id.activity_main_admin_btn);
         adminButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(v.getContext(),"admin activity",Toast.LENGTH_LONG).show();
                 Intent adminIntent = new Intent(MainActivity.this, AdminLoginActivity.class);
                 startActivity(adminIntent);
             }
         });
 
-        Button studentButton= (Button) findViewById(R.id.activity_main_student_btn);
+        CardView studentButton= (CardView) findViewById(R.id.activity_main_student_btn);
         studentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(v.getContext(),"student activity",Toast.LENGTH_LONG).show();
                 Intent studentIntent = new Intent(MainActivity.this, studentLoginActivity.class);
                 startActivity(studentIntent);
             }
