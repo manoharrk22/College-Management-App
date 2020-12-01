75935e0faf27aa5efd1f0cc6f2569981ab86990d
suprit <supriths4685@gmail.com>
2020-12-01 10:56:19 +0530

    firebase link to admin activity

--- app/src/main/java/com/example/collegemanagementBmsit/AdminLoginActivity.java
+++ app/src/main/java/com/example/collegemanagementBmsit/AdminLoginActivity.java
@@ -4,29 +4,111 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class AdminLoginActivity extends AppCompatActivity {
+    public EditText emailid, password;
+    CardView signin;
+    Button signup;
+    FirebaseAuth mfirebaseauth;
+    private FirebaseAuth.AuthStateListener mauthstatelistener;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_login);
-
-        CardView adminLoginBtn = (CardView) findViewById(R.id.admin_login_login_btn);
-        Button register=(Button)findViewById(R.id.button1);
-        adminLoginBtn.setOnClickListener(new View.OnClickListener() {
+        mfirebaseauth = FirebaseAuth.getInstance();
+        emailid = findViewById(R.id.admin_login_username_edittext);
+        password = findViewById(R.id.admin_login_password_edittext);
+        signin = (CardView) findViewById(R.id.admin_login_login_btn);
+        signup=(Button)findViewById(R.id.button1);
+        mauthstatelistener = new FirebaseAuth.AuthStateListener() {
             @Override
-            public void onClick(View v) {
-                Intent adminLoginToCategoryIntent = new Intent(AdminLoginActivity.this, CategoryAdminActivity.class);
-                startActivity(adminLoginToCategoryIntent);
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mfirebaseuser = mfirebaseauth.getCurrentUser();
+                if (mfirebaseuser != null) {
+                    Toast.makeText(AdminLoginActivity.this, "you are logged in", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(AdminLoginActivity.this, CategoryAdminActivity.class);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(AdminLoginActivity.this, "plese login", Toast.LENGTH_SHORT).show();
+                }
             }
-        });
-        register.setOnClickListener(new View.OnClickListener(){
+        };
+        signin.setOnClickListener(new View.OnClickListener() {
+                                      @Override
+                                      public void onClick(View view) {
+                                          String email = emailid.getText().toString();
+                                          String pwd = password.getText().toString();
+                                          if (email.isEmpty()) {
+                                              emailid.setError("plese enter your email");
+                                              emailid.requestFocus();
+                                          } else if (pwd.isEmpty()) {
+                                              emailid.setError("plese enter your password");
+                                              emailid.requestFocus();
+                                          } else if (email.isEmpty() && pwd.isEmpty()) {
+                                              Toast.makeText(AdminLoginActivity.this, "fields are empty", Toast.LENGTH_SHORT).show();
+                                          } else if (!(email.isEmpty() && pwd.isEmpty())) {
+                                              mfirebaseauth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(AdminLoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                                  @Override
+                                                  public void onComplete(@NonNull Task<AuthResult> task) {
+                                                      if (!task.isSuccessful()) {
+                                                          Toast.makeText(AdminLoginActivity.this, "login error,please login again", Toast.LENGTH_SHORT).show();
+                                                      } else {
+                                                          Intent intocategory = new Intent(AdminLoginActivity.this, CategoryAdminActivity.class);
+                                                          startActivity(intocategory);
+                                                      }
+                                                  }
+                                              });
+                                          } else {
+                                              Toast.makeText(AdminLoginActivity.this, "error occured!!!", Toast.LENGTH_SHORT).show();
+                                          }
+                                      }
+                                  }
+        );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        signup.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Toast.makeText(AdminLoginActivity.this,"admin register",Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/collegemanagementBmsit/adminregister.java
+++ app/src/main/java/com/example/collegemanagementBmsit/adminregister.java
@@ -1,21 +1,65 @@
 package com.example.collegemanagementBmsit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class adminregister extends AppCompatActivity {
     Button backlogin;
+    EditText emailid, password;
+    CardView signup;
+    FirebaseAuth mfirebaseauth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adminregister);
         backlogin=(Button) findViewById(R.id.backtologin);
+        mfirebaseauth = FirebaseAuth.getInstance();
+        emailid = findViewById(R.id.textview1);
+        password = findViewById(R.id.textview2);
+        signup = findViewById(R.id.regiserbutton);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailid.getText().toString();
+                String pwd = password.getText().toString();
+                if (email.isEmpty()) {
+                    emailid.setError("plese enter your email");
+                    emailid.requestFocus();
+                } else if (pwd.isEmpty()) {
+                    password.setError("plese enter your password");
+                    password.requestFocus();
+                } else if (email.isEmpty() && pwd.isEmpty()) {
+                    Toast.makeText(adminregister.this, "fields are empty", Toast.LENGTH_SHORT).show();
+                } else if (!(email.isEmpty() && pwd.isEmpty())) {
+                    mfirebaseauth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(adminregister.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(adminregister.this, "signup unsuccessful", Toast.LENGTH_SHORT).show();
+                            } else {
+                                startActivity(new Intent(adminregister.this, CategoryStudentActivity.class));
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(adminregister.this, "error occured!!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         backlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
